4952e12e10955bdaf0123d00e39bf169b3741ae5
inhwan You <zswea12@gmail.com>
2022-01-23 14:12:10 +0900

    generic views

--- polls/urls.py
+++ polls/urls.py
@@ -3,13 +3,20 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'  #이름명시
+# urlpatterns = [
+#     #ex : /polls/
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     path('<int:question_id>/',views.detail, name='detail'),  # question_id 는 viwe에있는 question_id랑 일치하여야 한다.
+#     #ex : /polls/5/results/
+#     path('<int:question_id>/results/',views.results, name='results'),
+#     #ex : /polls/5/vote/
+#     path('<int:question_id>/vote/',views.vote, name='vote'),
+# ]
+
 urlpatterns = [
-    #ex : /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/',views.detail, name='detail'),  # question_id 는 viwe에있는 question_id랑 일치하여야 한다.
-    #ex : /polls/5/results/
-    path('<int:question_id>/results/',views.results, name='results'),
-    #ex : /polls/5/vote/
-    path('<int:question_id>/vote/',views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),          #2~3번째 경로가 pk로 됨
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'), #pk란 DB내의 하나의 열(하나의 데이터를)구분할수 있는 값
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -5,32 +5,33 @@ from django.http import HttpResponse,Http404 , HttpResponseRedirect
 from django.template import loader
 from polls.models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
-def index(request): #클라이언트로부터 리퀘스트를 받아서 index뷰를 호출
-    # return HttpResponse("Hello, world. You're at the polls index.") #response 해준다
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # # output = ', '.join([q.question_text for q in latest_question_list])
-    # # return HttpResponse(output)
-    # template = loader.get_template('polls/index.html')
-    # context = {  #context 를 통해서 데이터를 전달
-    #     'latest_question_list' : latest_question_list,
-    # }
-    # return HttpResponse(template.render(context,request))
+# def index(request): #클라이언트로부터 리퀘스트를 받아서 index뷰를 호출
+#     # return HttpResponse("Hello, world. You're at the polls index.") #response 해준다
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # # output = ', '.join([q.question_text for q in latest_question_list])
+#     # # return HttpResponse(output)
+#     # template = loader.get_template('polls/index.html')
+#     # context = {  #context 를 통해서 데이터를 전달
+#     #     'latest_question_list' : latest_question_list,
+#     # }
+#     # return HttpResponse(template.render(context,request))
 
-    #render 사용
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list' : latest_question_list}
-    return render(request, 'polls/index.html', context)
+#     #render 사용
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list' : latest_question_list}
+#     return render(request, 'polls/index.html', context)
 
 # def detail(request, question_id):
 #     return HttpResponse("you're looking at question %s." %question_id)
 
-def results(request, question_id):
-    # response = "you're looking at the results of question %s."
-    # return HttpResponse(response % question_id)
+# def results(request, question_id):
+#     # response = "you're looking at the results of question %s."
+#     # return HttpResponse(response % question_id)
 
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html', {'question': question})
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request,'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -53,11 +54,30 @@ def vote(request, question_id):
 # def vote(request, question_id):
 #     return HttpResponse("You're voting on question %s." % question_id)
 
-def detail(request,question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, 'polls/detail.html', {'question' : question})
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request, 'polls/detail.html',{'question':question})
+# def detail(request,question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question' : question})
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request, 'polls/detail.html',{'question':question})
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
